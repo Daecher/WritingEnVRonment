d2aed3ba2668c9d99110ceaf15c30346db5674d5
STUDENTS\kdhooks <kdhooks@ualr.edu>
2018-07-24 16:57:08 -0500

    Modified scene, added keystroke reading script

---
+++ Assets/KeystrokeReader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeystrokeReader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+        
+	}
+
+    private void OnGUI()
+    {
+        Event e = Event.current;
+        if (e != null && e.isKey)
+        {
+            Debug.Log("The detected keycode is: " + e.keyCode + ". The detected character is: " + e.character);
+            Debug.Log(e.character == '\0');
+        }
+    }
+}
